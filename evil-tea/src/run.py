b85abd7d27a688ef6b3f48e0a191e3285d34f033
QJoly <github@une-pause-cafe.fr>
2024-03-11 16:17:32 +0100

    Add static files and update index.html

--- evil-tea/src/run.py
+++ evil-tea/src/run.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from kubernetes import client, config
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 def get_all_pods():
     # Charger la configuration Kubernetes à partir du fichier kubeconfig par défaut
